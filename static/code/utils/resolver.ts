b42ab510e1f8cceae3267fbc92c2f1701247122d
Simon Reinisch <reinisch@weclapp.com>
2021-07-01 08:09:12 +0200

    #47 Make properties for address and contact resolver optional

--- static/code/utils/resolver.ts
+++ static/code/utils/resolver.ts
@@ -1,5 +1,5 @@
 interface IDObject {
-    id: string;
+    id?: string;
 }
 
 /**
@@ -7,10 +7,10 @@ interface IDObject {
  * @param party Source, can be a customer, party, lead or any similar type.
  */
 export const resolvePrimaryAddress = <T extends IDObject>(
-    party: {primaryAddressId: string; addresses: T[];}
+    party: {primaryAddressId?: string; addresses?: T[];}
 ): T | null => {
     const {primaryAddressId, addresses} = party;
-    return addresses.find(v => v.id === primaryAddressId) ?? null;
+    return !primaryAddressId ? null : addresses?.find(v => v?.id === primaryAddressId) ?? null;
 };
 
 /**
@@ -18,8 +18,8 @@ export const resolvePrimaryAddress = <T extends IDObject>(
  * @param party Source, can be a customer, party, lead or any similar type.
  */
 export const resolvePrimaryContact = <T extends IDObject>(
-    party: {primaryContactId: string; contacts: T[];}
+    party: {primaryContactId?: string; contacts?: T[];}
 ): T | null => {
     const {primaryContactId, contacts} = party;
-    return contacts.find(v => v.id === primaryContactId) ?? null;
+    return !primaryContactId ? null : contacts?.find(v => v?.id === primaryContactId) ?? null;
 };
