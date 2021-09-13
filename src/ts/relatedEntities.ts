dbbfedfb76130d7afdea543e878621474940c454
Oliver Lutz <lutz@weclapp.com>
2021-09-13 15:15:40 +0200

    #56 Use tsInterfaceProperties function to build references for relatedEntity

--- src/ts/relatedEntities.ts
+++ src/ts/relatedEntities.ts
@@ -1,5 +1,6 @@
 import {indent} from '@utils/indent';
 import {pascalCase} from 'change-case';
+import {tsInterfaceProperties} from '@ts/interfaces';
 
 export interface RelatedEntityProperty {
     property: string;
@@ -11,14 +12,26 @@ export const relatedEntitiesName = (entityName: string): string => {
 };
 
 const resolveReferencesForRelatedEntity = ({property, relatedEntity}: RelatedEntityProperty): string => {
-    return `
-        entity: ${pascalCase(relatedEntity)},
-        relatedEntity: ${relatedEntitiesName(pascalCase(relatedEntity))},
-        sortAndFilterProperty: '${property.endsWith('Id') ? property.slice(0, -2) : property}',
-    `;
+    return tsInterfaceProperties([
+        {
+            name: 'entity',
+            value: pascalCase(relatedEntity),
+            required: true
+        },
+        {
+            name: 'relatedEntity',
+            value: relatedEntitiesName(pascalCase(relatedEntity)),
+            required: true
+        },
+        {
+            name: 'sortAndFilterProperty',
+            value: `'${property.endsWith('Id') ? property.slice(0, -2) : property}'`,
+            required: true
+        }
+    ], 1);
 };
 
 export const relatedEntityPropertyDefinition = (relatedEntities: RelatedEntityProperty[], indentLevel?: number): string => {
-    const str = relatedEntities.map(v => `readonly ${v.property}: {${resolveReferencesForRelatedEntity(v)}};`).join('\n');
+    const str = relatedEntities.map(v => `readonly ${v.property}: {\n${resolveReferencesForRelatedEntity(v)}\n};`).join('\n');
     return indentLevel !== undefined ? indent(str, indentLevel) : str;
 };
