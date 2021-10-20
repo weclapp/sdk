bb63d8db7b648fa760384a5f57852e79ee7c69a4
Oliver Lutz <lutz@weclapp.com>
2021-10-20 09:57:48 +0200

    #61 Use and fix builds with including hidden endpoints

--- src/generator/library/generateLibraryRoot.ts
+++ src/generator/library/generateLibraryRoot.ts
@@ -53,6 +53,7 @@ export * from './types.api';
 // TODO: Remove after swagger.json is fixed
 type Body2 = any;
 type Body3 = any;
+type Body4 = any;
 
 // Current version.
 export const version = '${pkg.version}';
