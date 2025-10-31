8f7e4400321ac82e8b56b5c08181184e99990ae3
Olivier Costi <costi@weclapp.com>
2025-10-31 11:10:31 +0100

    feat: adjust generation of custom endpoints (#46)

--- src/generator/04-services/utils/generateGenericFunctionName.ts
+++ src/generator/04-services/utils/generateGenericFunctionName.ts
@@ -1,5 +1,9 @@
 import { camelCase } from 'change-case';
+import { RESERVERD_FUNCTION_NAMES as RESERVED_METHOD_NAMES } from '..';
 
 export const generateGenericFunctionName = (path: string, suffix = '', prefix = '') => {
-  return camelCase(`${prefix}_` + path.replace(/.*\//, '').replace(/\W+/, '_').replace(/[_]+/, '_') + `_${suffix}`);
+  const processedPath = path.replace(/.*\//, '').replace(/\W+/, '_').replace(/[_]+/, '_');
+  return camelCase(
+    `${!RESERVED_METHOD_NAMES.includes(processedPath) ? '' : prefix + '_'}` + processedPath + `_${suffix}`
+  );
 };
