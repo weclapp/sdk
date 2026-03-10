27020423fa56683cc3baa760b5cc91b539500963
Olivier Costi <olivier.costi@exact.com>
2026-03-10 10:54:16 +0100

    feat: improve support for included entity references

--- src/generator/04-services/endpoints/some.ts
+++ src/generator/04-services/endpoints/some.ts
@@ -1,17 +1,18 @@
-import { resolveResponseType } from '../../../target';
 import { GeneratedServiceFunction, ServiceFunctionGenerator } from '@generator/04-services/types';
 import { generateArrowFunction } from '@ts/generateArrowFunction';
 import { generateArrowFunctionType } from '@ts/generateArrowFunctionType';
 import { generateInterfaceFromObject, generateInterfaceType, InterfaceProperty } from '@ts/generateInterface';
+import { generateObject, ObjectProperty } from '@ts/generateObject';
 import { generateString } from '@ts/generateString';
+import { generateType } from '@ts/generateType';
 import { convertToTypeScriptType, createObjectType } from '@utils/openapi/convertToTypeScriptType';
 import { isObjectSchemaObject, isParameterObject, isResponseObject } from '@utils/openapi/guards';
 import { pascalCase } from 'change-case';
 import { OpenAPIV3 } from 'openapi-types';
-import { generateType } from '@ts/generateType';
+import { resolveResponseType } from '../../../target';
 import { GeneratedEntity } from '../../03-entities';
-import { resolveResponsesObject } from '../utils/resolveResponsesObject';
 import { resolveParameters } from '../utils/resolveParameters';
+import { resolveResponsesObject } from '../utils/resolveResponsesObject';
 
 const excludedParameters = [
   'page',
@@ -41,22 +42,107 @@ const resolveAdditionalPropertiesSchema = ({ responses }: OpenAPIV3.OperationObj
   return undefined;
 };
 
+const resolveArrayReferenceProperties = (
+  entity: string,
+  entities: Map<string, GeneratedEntity>,
+  visitedEntities = new Set<string>()
+): ObjectProperty[] => {
+  if (!entity || visitedEntities.has(entity)) {
+    return [];
+  }
+
+  const generatedEntity = entities.get(entity);
+  if (!generatedEntity) {
+    return [];
+  }
+
+  const nextVisitedEntities = new Set(visitedEntities);
+  nextVisitedEntities.add(entity);
+
+  const properties: ObjectProperty[] = [...generatedEntity.properties.entries()].flatMap(
+    ([property, propertyMetaData]): ObjectProperty[] => {
+      if (propertyMetaData.type === 'array') {
+        if (propertyMetaData.entity === 'onlyId') {
+          return [{ key: property, value: [{ key: 'id', value: 'string' }] }];
+        }
+
+        if (!propertyMetaData.entity) {
+          return [];
+        }
+
+        const nestedProperties = resolveArrayReferenceProperties(
+          propertyMetaData.entity,
+          entities,
+          nextVisitedEntities
+        );
+
+        if (!nestedProperties.length) {
+          return [];
+        }
+
+        return [{ key: property, value: nestedProperties }];
+      }
+
+      if (property.endsWith('Id')) {
+        return [{ key: property, value: 'string' }];
+      }
+
+      return [];
+    }
+  );
+
+  if (generatedEntity.parentName) {
+    properties.push(...resolveArrayReferenceProperties(generatedEntity.parentName, entities, nextVisitedEntities));
+  }
+
+  return properties;
+};
+
+// TODO: remove boolean from arrays in QuerySelect
 const resolveReferences = (entity: string, entities: Map<string, GeneratedEntity>) => {
   const references: InterfaceProperty[] = [];
   const generatedEntity = entities.get(entity);
   if (generatedEntity) {
+    if (entity === 'warehouseStock') {
+      console.log('generatedEntity', generatedEntity);
+    }
     for (const [property, propertyMetaData] of generatedEntity.properties) {
-      if (propertyMetaData.service) {
-        references.push({
-          name: property,
-          type: generateString(propertyMetaData.service),
-          required: true
-        });
+      // TODO: filter things like tags in opportunity
+      if (propertyMetaData.type === 'array') {
+        if (propertyMetaData.entity === 'onlyId') {
+          references.push({
+            name: property,
+            type: generateObject([{ key: 'id', value: 'string' }]),
+            required: true
+          });
+        } else {
+          const nestedProperties = resolveArrayReferenceProperties(propertyMetaData.entity || '', entities);
+          if (nestedProperties.length) {
+            references.push({
+              name: property,
+              type: generateObject(nestedProperties),
+              required: true
+            });
+          }
+        }
+      } else {
+        if (propertyMetaData.service) {
+          references.push({
+            name: property,
+            // TODO: maybe just string
+            type: generateString(propertyMetaData.service),
+            required: true
+          });
+        }
       }
     }
     if (generatedEntity.parentName) {
       references.push(...resolveReferences(generatedEntity.parentName, entities));
     }
+  }
+
+  if (entity === 'warehouseStock') {
+    console.log('references', references);
   }
   return references;
 };
@@ -114,6 +200,10 @@ export const generateSomeEndpoint: ServiceFunctionGenerator = ({
 
   const referencesTypeName = `${functionTypeName}_References`;
   const referencesTypeSource = generateInterfaceType(referencesTypeName, resolveReferences(endpoint.service, entities));
+
+  // if (endpoint.service === 'warehouseStock') {
+  //   console.log('referencesTypeSource', referencesTypeSource);
+  // }
 
   const additionalPropertyTypeName = `${functionTypeName}_AdditionalProperty`;
   const additionalPropertyTypeSource = generateType(additionalPropertyTypeName, 'string');
